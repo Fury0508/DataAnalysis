dca0692e40fc55a154e556b7b284e1e119f1644e
Dhananjay gupta <dhananjaygupta@Dhananjays-MacBook-Pro.local>
2022-09-13 13:48:56 +0530

    EDA zomato dataset

---
+++ multiAgrs.py
@@ -0,0 +1,11 @@
+def addme(*num):
+    # total = num +num2
+    # return total
+    total = 0
+    for n in num:
+        total = total +n
+    return total
+
+#myVal = addme(3,5)
+
+print(addme(3))
